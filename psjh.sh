d1f11c9370110cda11e5d4b94bc627a10069659c
Clemens Weninger <weninc@slac.stanford.edu>
2017-12-11 14:22:07 -0800

    move to production conda environment

--- psjh.sh
+++ psjh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export PATH=/reg/g/psdm/sw/conda/inst/miniconda2-prod-rhel7/bin:$PATH
-source activate ~weninc/.conda/envs/jhub
+#export PATH=/reg/g/psdm/sw/conda/inst/miniconda2-prod-rhel7/bin:$PATH
+#source activate jhub
 
 export PYTHONPATH=/reg/g/psdm/sw/jupyterhub/psjhub/jhub/jhub_remote_user_authenticator/
 exec jupyterhub
